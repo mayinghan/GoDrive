599648757a5e057227ee7a5c7213ffc4141e27be
“mayinghan” <ma.yingha@ufl.edu>
2020-01-14 23:19:55 -0500

    add sortbytime interface

--- utils/sortByTime.go
+++ utils/sortByTime.go
@@ -1,1 +1,31 @@
 package utils
+
+import (
+	"GoDisk/meta"
+	"time"
+)
+
+const base = "2006-01-02 15:04:05"
+
+// SortedByUploadTime : a slice of FileMetas sorted by uploaded time
+type SortedByUploadTime []meta.FileMeta
+
+/**
+Implement the comparator interface
+Len(), Swap(), Less()
+*/
+
+func (a SortedByUploadTime) Len() int {
+	return len(a)
+}
+
+func (a SortedByUploadTime) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a SortedByUploadTime) Less(i, j int) bool {
+	iTime, _ := time.Parse(base, a[i].UploadAt)
+	jTime, _ := time.Parse(base, a[j].UploadAt)
+
+	return iTime.UnixNano() > jTime.UnixNano()
+}
