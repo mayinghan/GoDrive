0f1bb550e89ba2bc25e8d6549de5ea4e998d8546
“mayinghan” <ma.yingha@ufl.edu>
2020-01-28 15:08:47 -0500

    add user-handler.go

---
+++ controller/user-handler.go
@@ -0,0 +1,12 @@
+package controller
+
+import (
+	"net/http"
+)
+
+// RegisterHandler handles user registration. Method: POST
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+
+	}
+}
--- db/user.go
+++ db/user.go
@@ -1,1 +1,34 @@
 package db
+
+import (
+	"GoDrive/db/mydb"
+	"fmt"
+)
+
+// UserRegister handles user registration. Return a bool and a server message
+func UserRegister(username string, password string) (bool, string, error) {
+	stmt, err := mydb.DBConn().Prepare(
+		"insert ignore into tbl_user (`username`, `password`) values(?, ?)")
+
+	if err != nil {
+		e := fmt.Sprint("Internal server error: Failed to insert to DB.")
+		fmt.Println(e + err.Error())
+		return false, e, err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(username, password)
+	if err != nil {
+		e := fmt.Sprint("Internal server error: Failed to insert to DB.")
+		fmt.Println(e + err.Error())
+		return false, e, err
+	}
+	// check how many row is affected
+	if ra, err := result.RowsAffected(); err == nil && ra > 0 {
+		return true, "Registered Successfully!", nil
+	} else if err == nil && ra <= 0 {
+		return false, "Failed to register. Duplicated user!", nil
+	} else {
+		return false, "Internal server error: Failed to insert to DB", err
+	}
+}
