fb47c7310c3b4aebaf8a85f11cbd5d0f8ee72c1b
“mayinghan” <ma.yingha@ufl.edu>
2020-02-05 15:34:43 -0500

    redis conn pool for email verify

--- cache/redis.go
+++ cache/redis.go
@@ -1,26 +1,61 @@
 package cache
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
 )
 
 var (
-	pool      *redis.Pool
-	redisHost = "127.0.0.1:6379"
-	redisPass = "123456"
+	emailVeriPool *redis.Pool
+	redisHost     = "127.0.0.1:6379"
+	redisPass     = "123456"
+	emailVeriDB   = 0
 )
 
-func emailVerifPool() *redis.Pool {
+func createEmailVerifPool() *redis.Pool {
 	return &redis.Pool{
 		MaxIdle:     50,
 		MaxActive:   30,
 		IdleTimeout: 300 * time.Second,
-		Dial: func (redis.Conn. error) {
+		Dial: func() (redis.Conn, error) {
 			// 1. open the connection
+			c, err := redis.Dial("tcp", redisHost)
+			if err != nil {
+				panic(err)
+			}
+
 			// 2. auth
+			if _, err = c.Do("AUTH", redisPass); err != nil {
+				c.Close()
+				fmt.Println("Redis auth failed")
+				return nil, err
+			}
 			// 3. select db 0
-		}
+			if _, err = c.Do("SELECT", emailVeriDB); err != nil {
+				c.Close()
+				fmt.Println("Select redis db for emailveri failed")
+				panic(err)
+				return nil, err
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(conn redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := conn.Do("PING")
+			return err
+		},
 	}
+}
+
+func init() {
+	emailVeriPool = createEmailVerifPool()
+}
+
+// EmailVeriPool returns the redis pool for email verification
+func EmailVeriPool() *redis.Pool {
+	return emailVeriPool
 }
