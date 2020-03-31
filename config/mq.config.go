48947c40bf986ae575e99e21d62eceb6f8ae2ffd
“mayinghan” <ma.yingha@ufl.edu>
2020-03-31 19:30:52 -0400

    Add rabbitmq config

---
+++ config/mq.config.go
@@ -0,0 +1,16 @@
+package config
+
+const (
+	// AsyncTransferEnable : whether enable mq when uploading to aws s3
+	AsyncTransferEnable = true
+	// RabbitURL : rabbitmq url
+	RabbitURL = "amqp://guest:guest@127.0.0.1:5672/"
+	// TransExchangeName : name of exchange
+	TransExchangeName = "upload-server.trans"
+	// TransS3QueueName : queue name for s3
+	TransS3QueueName = "upload-server.trans.aws"
+	// TransS3ErrQueueName : queue name for s3 error
+	TransS3ErrQueueName = "upload-server.trans.aws.err"
+	// TransS3RoutingKey : routing key for using aws s3
+	TransS3RoutingKey = "aws"
+)
